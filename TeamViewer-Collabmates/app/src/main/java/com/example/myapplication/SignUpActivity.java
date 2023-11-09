3955d1877367c1c5453320296cb6fecad62618eb
eyrogel <acu6621@students.uc-bcf.edu.ph>
2023-11-10 00:22:43 +0800

    Update Sign-in and added Registration

---
+++ TeamViewer-Collabmates/app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+        Button haveAccountButton = findViewById(R.id.signin_button);
+
+        haveAccountButton.setOnClickListener(view -> {
+            // Go back to MainActivity when the "Have Account" button is clicked
+            finish(); // This finishes the current activity (SignUpActivity) and goes back to the previous one (if any)
+        });
+
+
+    }
+}
+
